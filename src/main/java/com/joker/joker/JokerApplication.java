27a75052c56fd249b061fda7341e97dbf7d829f1
vikbo <vikbobinski@gmail.com>
2020-09-03 19:41:11 +0200

    Initial commit

---
+++ src/main/java/com/joker/joker/JokerApplication.java
@@ -0,0 +1,13 @@
+package com.joker.joker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joker/joker/JokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joker.joker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
